022b05bffcf4985a178965e266b50651989ac57d
krishna <krishnapatel.santophy@gmail.com>
2023-04-17 15:43:12 +0530

    allow null value to vendor_id

---
+++ database/migrations/2023_04_17_101139_add_nullable_vendor_idinquiries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inquiries', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('vendor_id')->nullable(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inquiries', function (Blueprint $table) {
+            //
+        });
+    }
+};
