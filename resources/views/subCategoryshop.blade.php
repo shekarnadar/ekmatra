f75766da65392e47ca9770803672900b5c25378a
krishna <krishnapatel.santophy@gmail.com>
2023-04-18 14:13:04 +0530

    subcategory filter

--- resources/views/subCategoryshop.blade.php
+++ resources/views/subCategoryshop.blade.php
@@ -17,83 +17,9 @@
                 <div class="container">
                 <div class="shop-content row gutter-lg">
                     <!-- Start of Sidebar, Shop Sidebar -->
-                    <aside class="sidebar shop-sidebar sticky-sidebar-wrapper sidebar-fixed">
-                        <!-- Start of Sidebar Overlay -->
-                        <div class="sidebar-overlay"></div>
-                        <a class="sidebar-close" href="#"><i class="close-icon"></i></a>
-
-                        <!-- Start of Sidebar Content -->
-                        <div class="sidebar-content scrollable">
-                            <!-- Start of Sticky Sidebar -->
-                            <div class="sticky-sidebar">
-                                <div class="filter-actions">
-                                    <label>Filter :</label>
-                                    <a href="#" class="btn btn-dark btn-link filter-clean">Clean All</a>
-                                </div>
-                                
-
-                                <!-- Start of Collapsible Widget -->
-                                <div class="widget widget-collapsible">
-                                    <h3 class="widget-title"><label>Price</label></h3>
-                                    <div class="widget-body">
-                                        <ul class="filter-items search-ul">
-                                            <li><a href="#">Under $10.00</a></li>
-                                            <li><a href="#">$10.00 - $50.00</a></li>
-                                            <li><a href="#">$50.00 - $100.00</a></li>
-                                            <li><a href="#">$100.00 - $500.00</a></li>
-                                        </ul>
-                                        <form class="price-range">
-                                            <input type="number" name="min_price" class="min_price text-center"
-                                                placeholder="$min"><span class="delimiter">-</span><input
-                                                type="number" name="max_price" class="max_price text-center"
-                                                placeholder="$max"><a href="#"
-                                                class="btn btn-primary btn-rounded">Go</a>
-                                        </form>
-                                    </div>
-                                </div>
-                                <!-- End of Collapsible Widget -->
-
-
-                                <!-- Start of Collapsible Widget -->
-                                @if(@$brand)
-                                <div class="widget widget-collapsible">
-                                    <h3 class="widget-title"><label>Brand</label></h3>
-                                    <ul class="widget-body filter-items item-check mt-1">
-                                        @foreach($brand as $val)
-                                        @if(@$val['feature_attributes'])
-                                                <li data-id="{{$val['feature_attributes']['id']}}"><a href="javascript:void(0)">{{$val['feature_attributes']['name']}}</a></li>
-                                        @endif
-                                        @endforeach
-                                    </ul>
-                                </div>
-                                @endif
-                                <!-- End of Collapsible Widget -->
-                                <div class="widget widget-collapsible">
-                                    <h3 class="widget-title"><label>Minimum qty</label></h3>
-                                    <ul class="widget-body filter-items item-check">
-                                        <li><a href="#">No minimum qty</a></li>
-                                        <li><a href="#">Under 50</a></li>
-                                        <li><a href="#">50-100</a></li>
-                                        <li><a href="#">100-150</a></li>
-                                        <li><a href="#">150 & up</a></li>
-                                    </ul>
-                                </div>
-
-                                <!-- Start of Collapsible Widget -->
-                                <div class="widget widget-collapsible">
-                                    <h3 class="widget-title"><label>Warranty</label></h3>
-                                    <ul class="widget-body filter-items item-check">
-                                        <li><a href="#">1 year</a></li>
-                                        <li><a href="#">2 years</a></li>
-                                        <li><a href="#">3 years</a></li>
-                                    </ul>
-                                </div>
-                                <!-- End of Collapsible Widget -->
-                            </div>
-                            <!-- End of Sidebar Content -->
-                        </div>
-                        <!-- End of Sidebar Content -->
-                    </aside>
+                    <input type="hidden" value="{{@$sub_cat['id']}}" id="subcat_id">
+                    <input type="hidden" value="{{@$sub_cat['category_id']}}" id="cat_id">
+                    @include('filter-view');
                     <!-- End of Shop Sidebar -->
 
                     <!-- Start of Main Content -->
@@ -105,97 +31,27 @@
                                         class="w-icon-category"></i><span>Filters</span></a>
                                 <div class="toolbox-item toolbox-sort select-box text-dark">
                                     <label>Sort By :</label>
-                                    <select name="orderby" class="form-control">
-                                        <option value="default" selected="selected">Default sorting</option>
-                                        <option value="popularity">Sort by popularity</option>
-                                        <option value="rating">Sort by average rating</option>
-                                        <option value="date">Sort by latest</option>
-                                        <option value="price-low">Sort by pric: low to high</option>
-                                        <option value="price-high">Sort by price: high to low</option>
+                                    <select name="orderby" class="form-control" id="orderby">
+                                        
+                                        <option value="created_at" data-order="desc"  selected>Sort by latest</option>
+                                        <option value="price" data-order="asc">Sort by pric: low to high</option>
+                                        <option value="price" data-order="desc">Sort by price: high to low</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="toolbox-right">
                                 <div class="toolbox-item toolbox-show select-box mr-0">
-                                    <select name="count" class="form-control">
-                                        <option value="9">Show 9</option>
-                                        <option value="12" selected="selected">Show 12</option>
-                                        <option value="24">Show 24</option>
-                                        <option value="36">Show 36</option>
+                                    <select name="limit_product" id="limit_product" class="form-control">
+                                        <option value="10" selected="selected">Show 10</option>
+                                        <option value="20">Show 20</option>
+                                        <option value="30">Show 30</option>
                                     </select>
                                 </div>
-                                <div class="toolbox-item toolbox-layout">
-                                    <a href="shop-boxed-banner.html" class="icon-mode-grid btn-layout active">
-                                        <i class="w-icon-grid"></i>
-                                    </a>
-                                    <a href="shop-list.html" class="icon-mode-list btn-layout">
-                                        <i class="w-icon-list"></i>
-                                    </a>
-                                </div>
+                                
                             </div>
                         </nav>
-                        <div class="product-wrapper row cols-lg-4 cols-md-3 cols-2">
-                            @foreach($product as $prod_val)
-                                <div class="product-wrap">
-                                    <div class="product product-image-gap product-simple">
-                                        <figure class="product-media">
-                                            <a href="{{url('product-detail/'.$prod_val['id'])}}">
-                                                <img src='{{url("product/".$prod_val['image'])}}' alt="Product" width="195" height="135" />
-                                                 <img src='{{url("product/".$prod_val['image'])}}' alt="Product" width="195" height="135" />
-                                            </a>
-                                           
-                                            <div class="product-action">
-                                                <a href="{{url('product-detail/'.$prod_val['id'])}}" class="btn-product" title="Quick View">Quick View</a>
-                                            </div>
-                                        </figure>
-                                        <div class="product-details">
-                                            
-                                            <h4 class="product-name">
-                                                <a href="{{url('product-detail/'.$prod_val['id'])}}">{{$prod_val['name']}}</a>
-                                            </h4>
-                                           
-                                            <div class="product-pa-wrapper">
-                                                <div class="product-price">
-                                                    <ins class="new-price">Price : {{$prod_val['price']}}</ins>
-                                                    <del class="old-price">{{$prod_val['mrp']}}</del>
-                                                </div>
-                                                <div class="product-price">
-                                                    <ins class="new-price"> Min Qty : {{$prod_val['maq']}}</ins>
-                                                </div>
-                                                <div class="product-action">
-                                                    <a href="#" class="btn-cart btn-product btn btn-link btn-underline">Add To Wishlist</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-
-                           
-                        </div>
-
-                        <div class="toolbox toolbox-pagination justify-content-between">
-                            <p class="showing-info mb-2 mb-sm-0">
-                                Showing<span>1-12 of 60</span>Products
-                            </p>
-                            <ul class="pagination">
-                                <li class="prev disabled">
-                                    <a href="#" aria-label="Previous" tabindex="-1" aria-disabled="true">
-                                        <i class="w-icon-long-arrow-left"></i>Prev
-                                    </a>
-                                </li>
-                                <li class="page-item active">
-                                    <a class="page-link" href="#">1</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">2</a>
-                                </li>
-                                <li class="next">
-                                    <a href="#" aria-label="Next">
-                                        Next<i class="w-icon-long-arrow-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
+                        <div id="tag_container">
+                            @include('presult')
                         </div>
                     </div>
                 </div>
@@ -209,3 +65,177 @@
 
 </x-guest-layout>
 
+<script type="text/javascript">
+  var brand_array = [];
+  var page_count = 1;
+  var warranty = '';
+  var max_price = '';
+  var min_price = '';
+  var min_qty = '';
+  var max_qty = '';
+  var page_limit = '';
+  var sort_by = 'created_at';
+  var order_by = 'desc';
+  var cat_id  = $("#cat_id").val();
+  var sub_cat_id  = $("#subcat_id").val();
+    $('.brand-item li').click(function(e){
+        event.preventDefault();
+      
+        var id = $(this).attr('data-id');
+        page_count = 1;
+    
+        var getClass = this.className;
+        if(getClass == 'active'){
+            removeBrand(id);
+        }else{
+                brand_array.push(id);       
+        }
+        getData(page_count);
+  });
+
+    $('#limit_product').change(function(e){
+            var id = $(this).val();
+            page_limit = id;
+            page_count = 1;
+            getData(page_count);
+    });
+  $('.price-item li').click(function(e){
+        
+        page_count = 1;
+        min_price = $(this).attr('data-minprice');
+        max_price = $(this).attr('data-maxprice');
+        var getClass = this.className;
+        if(getClass == 'active'){
+            min_price = '';
+            max_price = '';
+            
+            $('.price-item ul li').removeClass('active');
+        }else{
+            sort_by = "price"
+                order_by = "asc";
+            $('.price-item li').removeClass('active');
+                $(this).parent('.price-item li').addClass('active');
+        }
+        
+            getData(page_count);
+  });
+
+  function removeBrand(id){
+    brand_array.splice($.inArray(id, brand_array), 1);
+    return brand_array;
+  }
+    
+    $('.filter-clean').click(function(){
+        brand_array = [];
+    page_count = 1;
+    warranty = '';
+    max_price = '';
+    min_price = '';
+    min_qty = '';
+    max_qty = '';
+    page_limit = $('#page_limit').val();
+    sort_by = 'created_at';
+    order_by = 'desc';
+    getData(page_count);
+    });
+
+    $('.qty-item li').click(function(){
+        page_count = 1;
+
+        var id = $(this).attr('data-value');
+    
+        min_qty = $(this).attr('data-minqty');
+        max_qty = $(this).attr('data-maxqty');
+        var getClass = this.className;
+
+        if(getClass == 'active'){
+            $('.qty-item ul li').removeClass('active');
+            $(this).parent('.qty-item ul li').removeClass('active');
+            min_qty = '';
+        max_qty = '';
+        }else{
+            $('.qty-item li').removeClass('active');
+            $(this).parent('.qty-item li').addClass('active');
+        }
+        
+            getData(page_count);
+    });
+
+    $('.warranty-item li').click(function(){
+        page_count = 1;
+
+        var id = $(this).attr('data-value');
+      var getClass = this.className;
+
+        if(getClass == 'active'){
+            warranty = '';
+            $('.warranty-item ul li').removeClass('active');
+            $(this).parent('.warranty-item ul li').removeClass('active');
+        }else{
+            
+            warranty = id;
+            $('.warranty-item li').removeClass('active');
+            $(this).parent('.warranty-item li').addClass('active'); 
+            
+        }
+        getData(page_count);
+        
+
+    });
+
+    $(document).on('click', '.pagination a',function(event){
+            event.preventDefault();
+            $('.page-item li').removeClass('active');
+            $(this).parent('.page-item li').addClass('active');
+            var page = $(this).attr('data-value');
+            page_count = page;
+            getData(page_count);
+            event.preventDefault();
+    });
+
+    function getData(page){
+        $.ajax(
+        {
+                url:"{{route('filterResult')}}",
+                type: "Post",
+                async: false,
+                cache: false,
+                data :{
+                    'page' : page,
+                    'cat_id' : cat_id ,
+                    "_token": "{{ csrf_token() }}",
+                    "brand_array" : brand_array,
+                    "warranty" : warranty,
+                    "max_price" : max_price,
+                    "min_price" : min_price,
+                    "min_qty" : min_qty,
+                    "max_qty" : max_qty,
+                    "page_limit" : page_limit,
+                    "sort_by" : sort_by,
+                    "order_by" : order_by,
+                    "sub_cat_id" : sub_cat_id
+                },
+                datatype: "html"
+        }).done(function(data){
+                $("#tag_container").html(data);
+                 setTimeout(function () {
+                $('html, body').animate({
+                    scrollTop: $("#tag_container").offset().top - 500
+                }, 777);
+            }, 100);
+
+
+        }).fail(function(jqXHR, ajaxOptions, thrownError){
+                alert('No response from server');
+        });
+         return false;
+    }
+
+    $('#orderby').change(function(){
+        sort_by = $(this).val();
+        order_by = $(this).find(':selected').attr('data-order')
+        page_count = 1;
+        getData(page_count);
+    })
+
+</script>
