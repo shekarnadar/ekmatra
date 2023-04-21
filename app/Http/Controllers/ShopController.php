d68902539ee5a6336b25e2dd79ff4967bfc8b4c9
krishna <krishnapatel.santophy@gmail.com>
2023-04-21 13:54:44 +0530

    simple print

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -14,12 +14,13 @@ class ShopController extends Controller
 {
 	//
 	public function productList(Request $request){
-		$brand = Product::with('feature_attributes')->where('status',1)->groupBy('feature_attribute_id')->get();
+		echo "helllo";
+		// $brand = Product::with('feature_attributes')->where('status',1)->groupBy('feature_attribute_id')->get();
 
-    	$allcategory = 1;
-		$product = Product::where('status',1);
-		$product=$product->orderBy('created_at','desc')->paginate(10);
-		return view ('allproduct',compact('brand','product','allcategory'));
+    	// $allcategory = 1;
+		// $product = Product::where('status',1);
+		// $product=$product->orderBy('created_at','desc')->paginate(10);
+		// return view ('allproduct',compact('brand','product','allcategory'));
 		
 	}
 	public function index($category,Request $request){
