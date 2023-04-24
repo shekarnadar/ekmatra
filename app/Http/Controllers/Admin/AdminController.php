8e3c9c5c4d15687af0868ba9b84c7eb26c5a7ccd
krishna <krishnapatel.santophy@gmail.com>
2023-04-24 14:13:08 +0530

    dashboard changes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Inquiry;
+use App\Models\User;
+
 
 class AdminController extends Controller
 {
     //
     public function dashboard(){
-        return view('admin.dashboard');
+
+        $product_active = Product::where('status',1)->count();
+        $product_deactive = Product::where('status',0)->count();
+
+        $inquiry_rfq = Inquiry::where('type','rfq')->count();
+        $inquiry = Inquiry::where('type','enquiry')->count();
+
+        $vendor = getRole('vendor');
+        $customer = getRole('customer');
+
+        $total_vendor = User::where('role_id',$vendor)->count();
+        $total_customer = User::where('role_id',$customer)->count();
+
+        return view('admin.dashboard',compact('product_active','product_deactive','inquiry_rfq','inquiry','total_vendor','total_customer'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,85 @@
-@section('title', "Account Settings")
+@section('breadcumb','Dashboard')
+@section('pageTitle','Dashboard')
+
 <x-app-layout>
-	test
-</x-app-layout>	
+	<div class="container-fluid">
+				
+				
+				<div class="main-content-body">
+					<div class="row row-sm">
+						<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+							<div class="card overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+										
+										<div class="project-content">
+											<h6>Products</h6>
+											<ul>
+												<li>
+													<strong>Active</strong>
+													<span>{{$product_active}}</span>
+												</li>
+												<li>
+													<strong>DeActive</strong>
+													<span>{{$product_deactive}}</span>
+												</li>
+
+
+											
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+							<div class="card  overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+										
+										<div class="project-content">
+											<h6>Users</h6>
+											<ul>
+												<li>
+													<strong>Vendors</strong>
+													<span>{{$total_vendor}}</span>
+												</li>
+
+												<li>
+													<strong>Customers</strong>
+													<span>{{$total_customer}}</span>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+							<div class="card overflow-hidden project-card">
+								<div class="card-body">
+									<div class="d-flex">
+									
+										<div class="project-content">
+											<h6>Enquiry</h6>
+											<ul>
+												<li>
+													<strong>RFQ</strong>
+													<span>{{$inquiry_rfq}}</span>
+												</li>
+
+												<li>
+													<strong>Enquiry</strong>
+													<span>{{$inquiry}}</span>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+	</div>
+</x-app-layout>
