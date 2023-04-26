410c7b80783455a958768730320a495ba2492b8e
krishna <krishnapatel.santophy@gmail.com>
2023-04-26 19:25:08 +0530

    dashboard icon change

--- resources/views/vendor/dashboard.blade.php
+++ resources/views/vendor/dashboard.blade.php
@@ -1,39 +1,27 @@
 @section('breadcumb','Dashboard')
 @section('pageTitle','Dashboard')
-
+@php
+$product = $product_deactive + $product_active;
+@endphp
 <x-app-layout>
 	<div class="container-fluid">
 				
 				
 				<div class="main-content-body">
 					<div class="row row-sm">
-						<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
-							<div class="card overflow-hidden project-card">
+						<div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+							<div class="card">
 								<div class="card-body">
-									<div class="d-flex">
-										
-										<div class="project-content">
-											<h6>Products</h6>
-											<ul>
-												<li>
-													<strong>Active</strong>
-													<span>{{$product_active}}</span>
-												</li>
-												<li>
-													<strong>DeActive</strong>
-													<span>{{$product_deactive}}</span>
-												</li>
-
-
-											
-											</ul>
-										</div>
+									<div class="card-order">
+										<h6 class="mb-2">Products</h6>
+										<h2 class="text-right "><i class="mdi mdi-cube icon-size float-left text-success text-success-shadow"></i><span>{{$product}}</span></h2>
+										<p class="mb-0">Active<span class="float-right">{{$product_active}}</span></p>
+										<p class="mb-0">DeActive<span class="float-right">{{$product_deactive}}</span></p>
 									</div>
 								</div>
 							</div>
 						</div>
-						
-						
+				
 					</div>
 				</div>
 	</div>
