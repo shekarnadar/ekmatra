da9768bb67649b4928100b6dc04173ce8891994d
krishna <krishnapatel.santophy@gmail.com>
2023-04-27 12:23:41 +0530

    footer change

--- resources/views/layouts/guest-footer.blade.php
+++ resources/views/layouts/guest-footer.blade.php
@@ -130,7 +130,7 @@
 				</div>
 				<div class="footer-bottom">
 					<div class="footer-left">
-						<p class="copyright">Copyright © <?php echo date('Y') ?> Ekmatra Store. All Rights Reserved.</p>
+						<p class="copyright">Copyright © <?php echo date('Y') ?> Ekmatra. All Rights Reserved.</p>
 					</div>
 					<div class="footer-right">
 						<span class="payment-label mr-lg-8">We're using safe payment for</span>
