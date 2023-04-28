85f9445408e9f901615a36c1166e4a9384fda42a
krishna <krishnapatel.santophy@gmail.com>
2023-04-28 17:39:59 +0530

    browse catgory

--- resources/views/layouts/guest-header.blade.php
+++ resources/views/layouts/guest-header.blade.php
@@ -1,17 +1,17 @@
  <header class="header header-border">
-            <div class="header-top">
-                <div class="container">
-                    <div class="header-left">
-                        <p class="welcome-msg">Welcome to Ekmatra Store!</p>
-                    </div>
-                    <div class="header-right">
-                       
-                        <!-- End of DropDown Menu -->
+			<div class="header-top">
+				<div class="container">
+					<div class="header-left">
+						<p class="welcome-msg">Welcome to Ekmatra Store!</p>
+					</div>
+					<div class="header-right">
+					   
+						<!-- End of DropDown Menu -->
 
-                      
-                        <span class="divider d-lg-show"></span>
+					  
+						<span class="divider d-lg-show"></span>
 							<a href="{{url('vendor/login')}}" class="d-lg-show">Sell on Ekmatra</a>
-                        @auth
+						@auth
 								<a href="{{url('myaccount')}}" class="d-lg-show">My Account</a>
 							
 							@else
@@ -20,20 +20,20 @@
 								<span class="delimiter d-lg-show">/</span>
 									<a href="{{url('login')}}" class="ml-0 d-lg-show login register">Register</a>
 							@endauth
-                    </div>
-                </div>
-            </div>
-            <!-- End of Header Top -->
+					</div>
+				</div>
+			</div>
+			<!-- End of Header Top -->
 
-            <div class="header-middle">
-                <div class="container">
-                    <div class="header-left mr-md-4">
-                        <a href="#" class="mobile-menu-toggle  w-icon-hamburger" aria-label="menu-toggle">
-                        </a>
-                        <a href="demo1.html" class="logo ml-lg-0">
-                            <img src="{{url('front//images/demos/demo12/logo.png')}}" alt="logo" width="120" height="45" />
-                        </a>
-                        <form method="get" action="{{url('search')}}"
+			<div class="header-middle">
+				<div class="container">
+					<div class="header-left mr-md-4">
+						<a href="#" class="mobile-menu-toggle  w-icon-hamburger" aria-label="menu-toggle">
+						</a>
+						<a href="demo1.html" class="logo ml-lg-0">
+							<img src="{{url('front//images/demos/demo12/logo.png')}}" alt="logo" width="120" height="45" />
+						</a>
+						<form method="get" action="{{url('search')}}"
 									class="input-wrapper header-search hs-expanded hs-round d-none d-md-flex">
 									<div class="select-box bg-white">
 											<select id="category"  onchange="location = this.value;" id="select_cat_id">
@@ -49,17 +49,17 @@
 											<i class="w-icon-search"></i>
 									</button>
 							</form>
-                    </div>
-                    <div class="header-right ml-4">
-                        <div class="header-call d-xs-show d-lg-flex align-items-center">
-                            <a href="tel:#" class="w-icon-call"></a>
-                            <div class="call-info d-lg-show">
-                                <h4 class="chat font-weight-normal font-size-md text-normal ls-normal text-light mb-0">
-                                    <a href="https://portotheme.com/cdn-cgi/l/email-protection#daf9" class="text-capitalize">Live Chat</a> or :</h4>
-                                <a href="tel:#" class="phone-number font-weight-bolder ls-50">0(800)123-456</a>
-                            </div>
-                        </div>
-                        	<div class="dropdown cart-dropdown cart-offcanvas mr-0 mr-lg-1">
+					</div>
+					<div class="header-right ml-4">
+						<div class="header-call d-xs-show d-lg-flex align-items-center">
+							<a href="tel:#" class="w-icon-call"></a>
+							<div class="call-info d-lg-show">
+								<h4 class="chat font-weight-normal font-size-md text-normal ls-normal text-light mb-0">
+									<a href="https://portotheme.com/cdn-cgi/l/email-protection#daf9" class="text-capitalize">Live Chat</a> or :</h4>
+								<a href="tel:#" class="phone-number font-weight-bolder ls-50">0(800)123-456</a>
+							</div>
+						</div>
+							<div class="dropdown cart-dropdown cart-offcanvas mr-0 mr-lg-1">
 									<div class="cart-overlay"></div>
 									@auth
 
@@ -78,86 +78,119 @@
 									
 									<!-- End of Dropdown Box -->
 							</div>
-                      
-                    </div>
-                </div>
-            </div>
-            <!-- End of Header Middle -->
+					  
+					</div>
+				</div>
+			</div>
+			<!-- End of Header Middle -->
 
 
-            <div class="header-bottom sticky-content fix-top sticky-header">
-                <div class="container">
-                    <div class="inner-wrap">
-                        <div class="header-left">
-                            
-                            <nav class="main-nav">
-                                <ul class="menu active-underline">
-                                	<li>
-                                        <a href="{{url('/')}}">Home</a>
-                                    </li>
-                                    
-                                    <li>
-                                        <a href="shop-banner-sidebar.html">Shop By</a>
+			<div class="header-bottom sticky-content fix-top sticky-header">
+				<div class="container">
+					<div class="inner-wrap">
+						<div class="header-left">
+							 <div class="dropdown category-dropdown has-border" data-visible="true">
+								<a href="#" class="category-toggle" role="button" data-toggle="dropdown"
+									aria-haspopup="true" aria-expanded="true" data-display="static"
+									title="Browse Categories">
+									<i class="w-icon-category"></i>
+									<span>Browse Categories</span>
+								</a>
 
-                                        <!-- Start of Megamenu -->
-                                        <ul class="megamenu">
-                                            <li>
-                                                <h4 class="menu-title">Occasions</h4>
-                                                <ul>
-                                                    @foreach($occasions as $value)
-                                                    <li><a href="shop-banner-sidebar.html">{{$value['name']}}</a></li>
-                                                    @endforeach
-                                               </ul>
-                                            </li>
-                                            <li>
-                                                <h4 class="menu-title">Price</h4>
-                                                <ul>
-                                                    <li><a href="shop-grid-3cols.html">Under 100</a></li>
-                                                    <li><a href="shop-grid-4cols.html">100 t0 500</a></li>
-                                                    <li><a href="shop-grid-5cols.html">500 to 1000</a></li>
-                                                    <li><a href="shop-grid-6cols.html">1000 to 5000</a></li>
-                                                    <li><a href="shop-grid-6cols.html">5000 above</a></li>
-                                                    
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <h4 class="menu-title">Brand</h4>
-                                                <ul>
-                                                	@foreach($allFeature as $feature)
-                                                    <li><a href="product-variable.html">{{$feature['name']}}</a></li>
-                                                  @endforeach 
-                                                </ul>
-                                            </li>
-                                           
-                                        </ul>
-                                        <!-- End of Megamenu -->
-                                    </li>
-                                    <li>
-                                        <a href="{{url('/')}}">what we do</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{url('/')}}">Our work</a>
-                                    </li>
-                                     <li>
-                                        <a href="{{url('/')}}">About Us</a>
-                                    </li>
-                                     <li>
-                                        <a href="{{url('/')}}">We are hiring</a>
-                                    </li>
-                                   
-                                   
-                                </ul>
-                            </nav>
-                        </div>
-                        <div class="header-right">
-                           @auth
+								<div class="dropdown-box">
+									<ul class="menu vertical-menu category-menu">
+										@foreach($category as $cat)
+										<li>
+											<a href="{{url('shop/'.$cat['slug'])}}">
+												<i><img src="{{url('category/'.$cat['image'])}}" alt="Categroy" width="15" height="15" /></i>{{$cat['name']}}
+											</a>
+										@if(count($cat['subCategory']) > 0)
+											<ul>
+												@foreach($cat['subCategory'] as $subcat)
+												<li>
+															<a href="{{url('shop/'.$value['slug'].'/'.$subcat['slug'])}}">{{$subcat['name']}}</a>
+									
+												</li>
+												@endforeach
+												
+											
+											</ul>
+										@endif
+										</li>
+										@endforeach
+										
+									   
+										
+										
+									   
+									</ul>
+								</div>
+							</div>
+							<nav class="main-nav">
+								<ul class="menu active-underline">
+									
+									<li>
+										<a href="shop-banner-sidebar.html">Shop By</a>
+
+										<!-- Start of Megamenu -->
+										<ul class="megamenu">
+											<li>
+												<h4 class="menu-title">Occasions</h4>
+												<ul>
+													@foreach($occasions as $value)
+													<li><a href="shop-banner-sidebar.html">{{$value['name']}}</a></li>
+													@endforeach
+											   </ul>
+											</li>
+											<li>
+												<h4 class="menu-title">Price</h4>
+												<ul>
+													<li><a href="shop-grid-3cols.html">Under 100</a></li>
+													<li><a href="shop-grid-4cols.html">100 t0 500</a></li>
+													<li><a href="shop-grid-5cols.html">500 to 1000</a></li>
+													<li><a href="shop-grid-6cols.html">1000 to 5000</a></li>
+													<li><a href="shop-grid-6cols.html">5000 above</a></li>
+													
+												</ul>
+											</li>
+											<li>
+												<h4 class="menu-title">Brand</h4>
+												<ul>
+													@foreach($allFeature as $feature)
+													<li><a href="product-variable.html">{{$feature['name']}}</a></li>
+												  @endforeach 
+												</ul>
+											</li>
+										   
+										</ul>
+										<!-- End of Megamenu -->
+									</li>
+									<li>
+										<a href="{{url('/')}}">what we do</a>
+									</li>
+									<li>
+										<a href="{{url('/')}}">Our work</a>
+									</li>
+									 <li>
+										<a href="{{url('/')}}">About Us</a>
+									</li>
+									 <li>
+										<a href="{{url('/')}}">We are hiring</a>
+									</li>
+								   
+								   
+								</ul>
+							</nav>
+						</div>
+						<div class="header-right">
+						   @auth
 							<a class="d-lg-show mr-2 btn btn-dark btn-outline p15" style="padding:15px" href="{{url('submitanenquiry')}}">Request for Quotations</a >
 							@else
 								<a class="d-lg-show mr-2 btn btn-dark btn-outline sign-in requestforquotation p15" style="padding:15px" href="{{url('login')}}">Request for Quotations</a>
 							@endauth
-                            <a href="#" class="d-lg-show mr-2 btn btn-dark btn-outline p15" style="padding:15px" >Contact Us</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
+							<a href="#" class="d-lg-show mr-2 btn btn-dark btn-outline p15" style="padding:15px" >Contact Us</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
