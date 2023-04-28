ddcd9a0b41c182b287a0c230ba3156ee1c0b4b63
krishna <krishnapatel.santophy@gmail.com>
2023-04-28 18:27:50 +0530

    customer index

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,54 @@
+@section('breadcumb','Vendors')
+@section('pageTitle','vendors')
+@include('layouts.datatable-css')
+@php
+$url = getAuthGaurd();
+@endphp
+<x-app-layout>
+	<div class="row row-sm">
+		<div class="col-xl-12">
+						<div class="card mg-b-20">
+							
+							<div class="card-body">
+								<div class="table-responsive">
+									<table id="vendor-list" class="table key-buttons text-md-nowrap"></table>
+								</div>
+							</div>
+						</div>
+					</div>
+	</div>
+</x-app-layout>
+	@include('layouts.datatable-script');
+
+<script type="text/javascript">
+	var table ="";
+
+	table=$('#vendor-list').DataTable({
+		lengthChange: false,
+		processing: true,
+		serverSide: true,
+		paging:true,
+		language: {
+			searchPlaceholder: 'Search...',
+			sSearch: '',
+			infoFiltered:'',
+		},
+
+	
+		ajax: {
+				url: '{{ url("$url/customers") }}', // need to change here url
+				type: "GET",
+				async:false,
+		},
+		 columns: [
+
+			{data: 'name', name: 'name','title' : 'Name'},
+            {data: 'email', name: 'email' ,'title' : 'Email'},
+            {data: 'phone', name: 'phone' ,'title' : 'Phone'},
+          
+     ]
+	});
+
+	
+
+</script>
