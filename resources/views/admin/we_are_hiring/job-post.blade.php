b3a8c8bd207304f5b4ed16b2030f8bba6e8decb1
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-09 16:59:48 +0530

    job post blade

---
+++ resources/views/admin/we_are_hiring/job-post.blade.php
@@ -0,0 +1,58 @@
+@section('breadcumb','Job Post')
+@section('pageTitle','Job-post')
+@include('layouts.datatable-css')
+@php
+$url = getAuthGaurd();
+@endphp
+<x-app-layout>
+	<div class="row row-sm">
+		<div class="col-xl-12">
+						<div class="card mg-b-20">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mg-b-0 mt-2 mb-2">Job Post</h4>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+								
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table id="faq-list" class="table key-buttons text-md-nowrap"></table>
+								</div>
+							</div>
+						</div>
+					</div>
+	</div>
+</x-app-layout>
+	@include('layouts.datatable-script');
+<script type="text/javascript" src="{{url('backend/js/delete-data.js')}}"></script>
+
+<script type="text/javascript">
+	var table;
+	table = $('#faq-list').DataTable({
+		lengthChange: false,
+		processing: true,
+		serverSide: true,
+		paging:true,
+		ordering: false,
+		language: {
+			searchPlaceholder: 'Search...',
+			sSearch: '',
+			infoFiltered:'',
+		},
+	
+		ajax: {
+				url: '{{ url("$url/job-post") }}', // need to change here url
+				type: "GET",
+				async:false,
+		},
+		 columns: [
+            {data: 'name', name: 'name','title' : 'Name'},
+            {data: 'email', name: 'email','title' : 'Email'},
+            {data: 'number', name: 'number','title' : 'Phone'},
+            {data: 'action', name: 'action', orderable: false, searchable: false,title:'action'},
+     ]
+	});
+
+	
+</script>
