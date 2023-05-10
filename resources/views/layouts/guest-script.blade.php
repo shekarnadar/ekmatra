d8e4c587ca7df4e5f2f0ea7ff65efe61be6914f8
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 11:18:10 +0530

    demo.css

--- resources/views/layouts/guest-script.blade.php
+++ resources/views/layouts/guest-script.blade.php
@@ -48,7 +48,7 @@
 
 	<!-- Default CSS -->
      @if(request()->is('welcome') || request()->is('shop/*') || request()->is('search') ||  request()->is('shop-by/*'))
-     	 <link rel="stylesheet" type="text/css" href="{{url('front/css/demo1.min.css')}}"> 
+     	 <link rel="stylesheet" type="text/css" href="{{url('front/css/demo12.min.css')}}"> 
      
      @else
      		    <link rel="stylesheet" type="text/css" href="{{url('front/css/style.min.css')}}">
