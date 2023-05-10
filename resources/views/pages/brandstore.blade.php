5f80322b28c90a87ff6444432c9d60dc6b861e07
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 13:16:45 +0530

    brandstore design

--- resources/views/pages/brandstore.blade.php
+++ resources/views/pages/brandstore.blade.php
@@ -9,53 +9,78 @@
 					</ul>
 				</div>
 			</nav>
-			 <div class="page-content">
-				<div class="container">
-					<section class="introduce mb-2 pb-2">
-						<h2 class="title title-center">
-							Launch Your Company Brandstore Today,
-						</h2>
-						<p class=" mx-auto text-center">We are your go-to platform for launching a customised Brand Store. Here are some of the benefits of launching a Brand Store with us:</p>
-						<figure class="br-lg text-center">
-							<img src="{{url('whatwedo/brandstore.avif')}}" alt="Banner" 
-								style="background-color: #D0C1AE;" />
-						</figure>
-					</section>
+			 
+			 <div class="page-content become-a-vendor">
+                <div class="container create-store pb-1 pb-lg-5 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-6 mb-4 mb-md-0">
+                            <figure class="br-sm">
+                                <img src="{{url('whatwedo/brandstore.avif')}}" alt="Banner"
+                                    width="610" height="435" style="background-color: #D9D8DD;" />
+                            </figure>
+                        </div>
+                        <div class="col-md-6 order-md-first">
+                            <h4 class="text-primary font-weight-bold ls-25">Launch Your Company Brandstore Today,</h4>
+                            
+                            <p> We are your go-to platform for launching a customised Brand Store. Here are some of the benefits of launching a Brand Store with us:
+                            </p>
+                            <ul>
+																<li>Strengthen your brand identity with high-quality, branded merchandise</li>
+																<li>Build customer and employee loyalty with rewards and incentives</li>
+																<li>Drive sales with customised sales incentives</li>
+																<li>Cost-effective solution with a wide range of merchandise options to fit any budget</li>
+																 <li>Real-time data insights to make data-driven decisions</li>
+													</ul>
+                        </div>
+                    </div>
+                    <!-- End of Row -->
+                </div>
+                <!-- End of Create Store -->
+                 <div class="bg-grey how-trade pt-5 pb-5 pb-lg-5">
+                    <div class="container mt-2 mt-lg-5 mb-0 mb-lg-5">
+                        <div class="row align-items-center">
+                        	  <div class="col-lg-6 pl-lg-8">
+                                <h4 class="text-primary font-weight-bold ls-25">Launch a Brand Store for:</h4>
+                               
+                                <div class="row cols-sm-2 mb-1">
+                                    <div class="stage-item mb-6 stage-register d-flex align-items-center">
+                                        
+                                        <p class="mb-0 font-weight-bold">Employee rewards and recogincludenition programs</p>
+                                    </div>
+                                    <div class="stage-item mb-6 stage-selling d-flex align-items-center">
+                                       
+                                        <p class="mb-0 font-weight-bold">Sales incentives</p>
+                                    </div>
+                                    <div class="stage-item mb-5 stage-deliver d-flex align-items-center">
+                                       
+                                        <p class="mb-0 font-weight-bold">Customer loyalty programs</p>
+                                    </div>
+                                    <div class="stage-item mb-5 stage-get d-flex align-items-center">
+                                       
+                                        <p class="mb-0 font-weight-bold">Channel Partner incentive programs</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 mb-4 mb-lg-0">
+                               <p class="mb-6">
+                               	Our experienced team will ensure a smooth and satisfying experience for your employees and customers. Customise your Brand Store with personalised messaging and branded merchandise that showcases your values and brand identity.
 
-					<section class="introduce mb-2 pb-2 ">
-						
-							<ul>
-								<li>Strengthen your brand identity with high-quality, branded merchandise</li>
-								<li>Build customer and employee loyalty with rewards and incentives</li>
-								<li>Drive sales with customised sales incentives</li>
-								<li>Cost-effective solution with a wide range of merchandise options to fit any budget</li>
-								 <li>Real-time data insights to make data-driven decisions</li>
-							</ul>
-					</section>
 
-						<section class="introduce mb-2 pb-2 ">
-							<p>Launch a Brand Store for:</p>
-							<ul>
-								<li>Employee rewards and recogincludenition programs</li>
-								<li>Sales incentives</li>
-								<li>Customer loyalty programs</li>
-								<li>Channel Partner incentive programs</li>
-							</ul>
+                               </p>
+                               <p class="mb-6">
+                               	Let us help you create a rewarding and engaging experience for your employees and customers, while reinforcing your brand identity and driving sales.
 
-							<p>Our experienced team will ensure a smooth and satisfying experience for your employees and customers. Customise your Brand Store with personalised messaging and branded merchandise that showcases your values and brand identity.</p>
 
-							<p>Let us help you create a rewarding and engaging experience for your employees and customers, while reinforcing your brand identity and driving sales.</p>
-					</section>
 
-					
-
-				   
-				</div>
-
-			   
-
-			   
-			</div>
+                               </p>
+                            </div>
+                          
+                        </div>
+                        <!-- End of Row -->
+                    </div>
+                    <!-- End of Container -->
+                </div>
+      </div>
 </x-guest-layout>
 <script type="text/javascript">
     $('.whatwedo').addClass('active');
