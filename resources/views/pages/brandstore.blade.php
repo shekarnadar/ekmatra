c8f63ab4e1b9a02bf4b225357e82a3fc3adffab9
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 13:29:52 +0530

    brand store chnages

--- resources/views/pages/brandstore.blade.php
+++ resources/views/pages/brandstore.blade.php
@@ -11,9 +11,9 @@
 			</nav>
 			 
 			 <div class="page-content become-a-vendor">
-                <div class="container create-store pb-1 pb-lg-5 mb-5">
+                <div class="container create-store">
                     <div class="row align-items-center">
-                        <div class="col-md-6 mb-4 mb-md-0">
+                        <div class="col-md-6  mb-md-0">
                             <figure class="br-sm">
                                 <img src="{{url('whatwedo/brandstore.avif')}}" alt="Banner"
                                     width="610" height="435" style="background-color: #D9D8DD;" />
@@ -36,32 +36,17 @@
                     <!-- End of Row -->
                 </div>
                 <!-- End of Create Store -->
-                 <div class="bg-grey how-trade pt-5 pb-5 pb-lg-5">
-                    <div class="container mt-2 mt-lg-5 mb-0 mb-lg-5">
+                 <div class="how-trade pb-5 pb-lg-5">
+                    <div class="container  mb-0 mb-lg-5">
                         <div class="row align-items-center">
-                        	  <div class="col-lg-6 pl-lg-8">
+                        	  <div class="col-lg-12 pl-lg-8">
                                 <h4 class="text-primary font-weight-bold ls-25">Launch a Brand Store for:</h4>
-                               
-                                <div class="row cols-sm-2 mb-1">
-                                    <div class="stage-item mb-6 stage-register d-flex align-items-center">
-                                        
-                                        <p class="mb-0 font-weight-bold">Employee rewards and recogincludenition programs</p>
-                                    </div>
-                                    <div class="stage-item mb-6 stage-selling d-flex align-items-center">
-                                       
-                                        <p class="mb-0 font-weight-bold">Sales incentives</p>
-                                    </div>
-                                    <div class="stage-item mb-5 stage-deliver d-flex align-items-center">
-                                       
-                                        <p class="mb-0 font-weight-bold">Customer loyalty programs</p>
-                                    </div>
-                                    <div class="stage-item mb-5 stage-get d-flex align-items-center">
-                                       
-                                        <p class="mb-0 font-weight-bold">Channel Partner incentive programs</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 mb-4 mb-lg-0">
+                                <ul>
+                                	<li>Employee rewards and recogincludenition programs</li>
+                                	<li>Sales incentives</li>
+                                	<li>Customer loyalty programs</li>
+                                	<li>Channel Partner incentive programs</li>
+                                </ul>
                                <p class="mb-6">
                                	Our experienced team will ensure a smooth and satisfying experience for your employees and customers. Customise your Brand Store with personalised messaging and branded merchandise that showcases your values and brand identity.
 
@@ -73,7 +58,10 @@
 
 
                                </p>
+
+                                
                             </div>
+                           
                           
                         </div>
                         <!-- End of Row -->
