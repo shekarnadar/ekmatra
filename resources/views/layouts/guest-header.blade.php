51cd88397f994819e6c04f8670a672ac905b3aab
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 12:08:52 +0530

    chnage menu

--- resources/views/layouts/guest-header.blade.php
+++ resources/views/layouts/guest-header.blade.php
@@ -98,14 +98,14 @@
 								</a>
 
 								<div class="dropdown-box">
-									<ul class="menu vertical-menu category-menu">
+									<ul class="menu active-underline category-menu">
 										@foreach($category as $cat)
 										<li>
 											<a href="{{url('shop/'.$cat['slug'])}}">
 												<i><img src="{{url('category/'.$cat['image'])}}" alt="Categroy" width="15" height="15" /></i>{{$cat['name']}}
 											</a>
 										@if(count($cat['subCategory']) > 0)
-											<ul class="subcatmenu">
+											<ul >
 												@foreach($cat['subCategory'] as $subcat)
 												<li>
 															<a href="{{url('shop/'.$value['slug'].'/'.$subcat['slug'])}}">{{$subcat['name']}}</a>
