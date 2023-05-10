c3a8876c178eade46caac09111628a689021031c
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 15:42:19 +0530

    send design

--- resources/views/pages/send.blade.php
+++ resources/views/pages/send.blade.php
@@ -1,52 +1,60 @@
 <x-guest-layout>
-    <nav class="breadcrumb-nav">
-                <div class="container">
-                    <ul class="breadcrumb bb-no">
-                        <li><a href="{{url('/')}}">Home</a></li>
-                        <li><a href="javascript:void(0)">What We Do</a></li>
-                        <li><a href="javascript:void(0)">Send</a></li>
+	<style type="text/css">
+		li,p{
+			font-size: 15px !important;
+		}
+	</style>
+	<nav class="breadcrumb-nav">
+				<div class="container">
+					<ul class="breadcrumb bb-no">
+						<li><a href="{{url('/')}}">Home</a></li>
+						<li><a href="javascript:void(0)">What We Do</a></li>
+						<li><a href="javascript:void(0)">Send</a></li>
 
-                    </ul>
-                </div>
-            </nav>
-             <div class="page-content">
-                <div class="container">
-                    <section class="introduce mb-2 pb-2">
-                       
-                        <p class=" mx-auto text-center">SEND is the ultimate platform for enhancing your sending strategy and driving demand and revenue growth.</p>
-                        <figure class="br-lg text-center">
-                            <img src="{{url('whatwedo/send.avif')}}" alt="Banner" 
-                                style="background-color: #D0C1AE;" />
-                        </figure>
-                    </section>
+					</ul>
+				</div>
+			</nav>
+			 
+			 <div class="page-content become-a-vendor">
+				<div class="container create-store">
+					<div class="row align-items-center">
+						<div class="col-md-6  mb-md-0">
+							<figure class="br-sm">
+								<img src="{{url('whatwedo/send.avif')}}" alt="Banner"
+									width="610" height="435" style="background-color: #D9D8DD;" />
+							</figure>
+						</div>
+						<div class="col-md-6 order-md-first">
+							<h3 class="text-primary font-weight-bold ls-25">SEND is the ultimate platform </h3>
+							<h4 class=" text-left"> for enhancing your sending strategy <br>and driving demand and revenue growth.</h4>
+							
+							<p>Grow Your demand. Enhance Your revenue.</p>
+							<p>With SEND, you can leverage the power of corporate gifting to achieve steady pipeline growth and proven returns on marketing, while delivering fully measurable ROI. Our platform enables you to stand out from the competition with authentic engagement that builds true connections.</p>
 
-                    <section class="introduce mb-2 pb-2 ">
-                        <p>Grow Your demand. Enhance Your revenue.</p>
-                       
-                        <p>With SEND, you can leverage the power of corporate gifting to achieve steady pipeline growth and proven returns on marketing, while delivering fully measurable ROI. Our platform enables you to stand out from the competition with authentic engagement that builds true connections.</p> 
-                       
-                        <p>One platform benefits many teams</p>
-
-                        <p>SEND is a versatile platform that benefits many teams within your organization. Whether it's your sales team, customer success, marketing, or HR, our platform drives positive engagement throughout the customer lifecycle, resulting in greater employee engagement and a positive impact on your bottom line.</p> 
-
-                        <p>Corporate gifting drives positive engagement throughout the customer lifecycle.</p>
-
-                        <p>By using SEND, you can deliver personalized, high-quality gifts that make a lasting impression on your customers and employees, while also streamlining your sending process for maximum efficiency.</p>
-
-                        <p>At SEND, we offer a user-friendly and customizable platform that is tailored to meet your unique needs. Our team of experienced professionals is dedicated to providing you with the support you need to ensure a smooth and effective sending strategy, so you can focus on growing your business.</p>
-
-                        <p>Let us help you take your gifting and rewards program to the next level, while enhancing your revenue and driving demand growth.</p>
-                    </section>
-
-                    
-                   
-                </div>
-
-               
-
-               
-            </div>
+							<p>One platform benefits many teams</p>
+							<p>SEND is a versatile platform that benefits many teams within your organization. Whether it's your sales team, customer success, marketing, or HR, our platform drives positive engagement throughout the customer lifecycle, resulting in greater employee engagement and a positive impact on your bottom line.</p>
+							
+						</div>
+					</div>
+					<!-- End of Row -->
+				</div>
+				<!-- End of Create Store -->
+				 <div class="container create-store">
+					<div class="row align-items-center">
+							  <div class="row">
+									<p>Corporate gifting drives positive engagement throughout the customer lifecycle.</p>
+								   <p class="mb-2">
+									By using SEND, you can deliver personalized, high-quality gifts that make a lasting impression on your customers and employees, while also streamlining your sending process for maximum efficiency.</p>
+								   <p>At SEND, we offer a user-friendly and customizable platform that is tailored to meet your unique needs. Our team of experienced professionals is dedicated to providing you with the support you need to ensure a smooth and effective sending strategy, so you can focus on growing your business.</p>
+								   <p>Let us help you take your gifting and rewards program to the next level, while enhancing your revenue and driving demand growth.</p>
+								   <p>Contact us for a <a href="www.send1.in" target="_blank">quick Demo</a></p>
+								</div>
+						<!-- End of Row -->
+					</div>
+					<!-- End of Container -->
+				</div>
+	  </div>
 </x-guest-layout>
 <script type="text/javascript">
-    $('.whatwedo').addClass('active');
+	$('.whatwedo').addClass('active');
 </script>
