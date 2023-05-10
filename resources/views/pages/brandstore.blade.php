5f571df9ff7a211e8528f7369760ec2edf6e1dfa
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-10 13:39:22 +0530

    font size set

--- resources/views/pages/brandstore.blade.php
+++ resources/views/pages/brandstore.blade.php
@@ -1,4 +1,9 @@
 <x-guest-layout>
+	<style type="text/css">
+		li,p{
+			font-size: 15px !important;
+		}
+	</style>
 	<nav class="breadcrumb-nav">
 				<div class="container">
 					<ul class="breadcrumb bb-no">
@@ -20,7 +25,7 @@
                             </figure>
                         </div>
                         <div class="col-md-6 order-md-first">
-                            <h4 class="text-primary font-weight-bold ls-25">Launch Your Company Brandstore Today,</h4>
+                            <h3 class="text-primary font-weight-bold ls-25">Launch Your Company Brandstore Today,</h4>
                             
                             <p> We are your go-to platform for launching a customised Brand Store. Here are some of the benefits of launching a Brand Store with us:
                             </p>
@@ -36,28 +41,30 @@
                     <!-- End of Row -->
                 </div>
                 <!-- End of Create Store -->
-                 <div class="how-trade pb-5 pb-lg-5">
+                 <div class="">
                     <div class="container  mb-0 mb-lg-5">
-                        <div class="row align-items-center">
-                        	  <div class="col-lg-12 pl-lg-8">
-                                <h4 class="text-primary font-weight-bold ls-25">Launch a Brand Store for:</h4>
+                        <div class=" align-items-center">
+                        	  <div class="row pl-lg-8">
+                                <h3 class="text-primary font-weight-bold ls-25">Launch a Brand Store for,</h4>
+                            
                                 <ul>
                                 	<li>Employee rewards and recogincludenition programs</li>
                                 	<li>Sales incentives</li>
                                 	<li>Customer loyalty programs</li>
                                 	<li>Channel Partner incentive programs</li>
                                 </ul>
-                               <p class="mb-6">
+                               <p class="mb-2">
                                	Our experienced team will ensure a smooth and satisfying experience for your employees and customers. Customise your Brand Store with personalised messaging and branded merchandise that showcases your values and brand identity.
 
 
                                </p>
-                               <p class="mb-6">
+                               <p class="mb-2">
                                	Let us help you create a rewarding and engaging experience for your employees and customers, while reinforcing your brand identity and driving sales.
 
 
 
                                </p>
+                               <p>Contact us for a <a href="https://calendly.com/sendsoftware/30min" target="_blank">quick Demo</a></p>
 
                                 
                             </div>
