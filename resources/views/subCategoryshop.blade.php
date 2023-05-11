ab8e9c4ebbb4f3070e3767b457af33fce935ee43
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-11 17:48:14 +0530

    cat slug change

--- resources/views/subCategoryshop.blade.php
+++ resources/views/subCategoryshop.blade.php
@@ -7,7 +7,7 @@
                 <div class="container">
                     <ul class="breadcrumb bb-no">
                         <li><a href="{{url('/')}}">Home</a></li>
-                        <li><a href="{{url('shop/'.$cat_name)}}">{{$cat_name}}</a></li>
+                        <li><a href="{{url('shop/'.$cat_slug)}}">{{$cat_name}}</a></li>
                         <li>{{$subcategory_name}}</li>
                     </ul>
                 </div>
