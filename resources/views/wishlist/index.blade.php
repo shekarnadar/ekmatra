25fb014b0431a7f60dfce41c5d071bd6c7a1b589
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-12 17:58:15 +0530

    change wishlist

--- resources/views/wishlist/index.blade.php
+++ resources/views/wishlist/index.blade.php
@@ -1,9 +1,5 @@
 <x-guest-layout>
-	<div class="page-header">
-		<div class="container">
-			<h1 class="page-title mb-0">Wishlist</h1>
-		</div>
-	</div>
+	
 	<nav class="breadcrumb-nav mb-10">
 		<div class="container">
 			<ul class="breadcrumb">
