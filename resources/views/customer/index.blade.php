4b24d4183d055cab6a1c1310fc8777e9af83fd9e
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-16 16:06:44 +0530

    product active deactive

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -41,7 +41,12 @@ $url = getAuthGaurd();
 				async:false,
 		},
 		 columns: [
-
+		 	{
+        			"title": "Serial",
+        			render: function (data, type, row, meta) {
+        				return meta.row + meta.settings._iDisplayStart + 1;
+        			}
+        	},
 			{data: 'name', name: 'name','title' : 'Name'},
             {data: 'email', name: 'email' ,'title' : 'Email'},
             {data: 'phone', name: 'phone' ,'title' : 'Phone'},
