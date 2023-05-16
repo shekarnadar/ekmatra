af81ce704812697177d76e49c6fa3ec035023346
krishna patel <krishnapatel.santophy@gmail.com>
2023-05-16 12:53:47 +0530

    hide show

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -75,7 +75,7 @@ input:checked + .slider:before {
 								<div class="d-flex justify-content-between">
 									<h4 class="card-title mg-b-0 mt-2 mb-2">Product</h4>
 									<div style="float: right;">
-									<a href="javascript:void(0)" class="activeinactive btn btn-primary">Status Change</a>
+									<a href="javascript:void(0)" class="activeinactive btn btn-primary" style="display:none">Status Change</a>
 									<a href='{{url("$url/product-import")}}' class="btn btn-primary">Product Import</a>
 									<a href='{{url("$url/product/add")}}' class="btn btn-primary">Add Product</a>
 									<a href='{{url("$url/product/image")}}' class="btn btn-primary">Upload Image</a>
@@ -166,7 +166,13 @@ $("#product-list").on('change',".activeproducts",function(e){
 	});
 
 $("#product-list").on('change',".allCheckbox",function(e){
-	            $(".activeproducts").attr('checked', this.checked);
+	
+	 if (!$(this).prop("checked")) {
+	 	$('.activeinactive').hide();
+	 }else{
+	 	$('.activeinactive').show();
+	 }
+	$(".activeproducts").attr('checked', this.checked);
 
 });
 
