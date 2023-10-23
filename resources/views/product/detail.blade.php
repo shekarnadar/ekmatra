d69625780fb41ae99fd83d6259dd70b0ffc14156
Shekar <shekar.ndr@gmail.com>
2023-10-23 11:52:20 +0530

    Changes in product detail page

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -100,7 +100,7 @@
 											<div class="product-form container ">
 												@auth
 												<a href="javascript:void(0)"
-													class="addwishlist btn btn-dark btn-rounded mb-2 mb-lg-0" data-id="{{$product['id']}}">Add To Wishlist&nbsp;&nbsp;</a>
+													class="addwishlist btn btn-dark btn-rounded mb-2 mb-lg-0" data-id="{{$product['id']}}" data-price="{{$product['price']}}">Add To Wishlist&nbsp;&nbsp;</a>
 												@else
 														<a href="{{url('login')}}"
 													class="btn btn-dark btn-rounded sign-in mb-2 mb-lg-0" data-id="{{$product['id']}}">Add To Wishlist&nbsp;&nbsp;</a>
