b371e3982c0204495d4fc141e671de22f3bbaf97
shekarnadar <shekarndr@gmail.com>
2025-04-08 14:39:16 +0530

    small changes in header

--- resources/views/layouts/guest-header.blade.php
+++ resources/views/layouts/guest-header.blade.php
@@ -12,7 +12,7 @@ $user_id=@auth()->user()->id;
 	<div class="header-top">
 		<div class="container">
 			<div class="header-left">
-				<p class="welcome-msg">Welcome to Ekmatra Stores!</p>
+				<p class="welcome-msg">Welcomes to Ekmatra Stores!</p>
 			</div>
 			<div class="header-right">
 
